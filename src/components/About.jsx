eab2c26ea88b321a6de9922d315320a1e6ea1bdc
brianskim27 <brianskim27@gmail.com>
2025-05-27 17:50:50 -0400

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,10 +9,11 @@ function About() {
         <p>Hey there! I'm Brian, a '25 BSCS student at Binghamton University.</p>
         <p>
           Growing up, I've always enjoyed getting my hands on technology, and after initially testing the waters with the field of law, 
-          I decided to pursue the ever-growing computer  world.
+          I decided to pursue the ever-growing computer world. Lately, I've also been gaining a lot of interest in data analysis, accruing a lot of experience
+          in spreadsheets and numbers. I am always open to learning and growing more - the sky is the limit!
         </p>
         <p>
-          As the youngest in the family, I always strive to work hard and be unique, aiming to pave my own path rather than following my siblings' steps.
+          As the youngest in the family, I always strive to work hard and be unique, aiming to pave my own path.
           Ultimately, I want to be a difference to my environment and support the people around me, especially my loved ones.
         </p>
         <p>
